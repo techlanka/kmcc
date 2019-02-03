f42c2066bbff1ff08ca767d95e869f1335a11773
IT <it@masskar.com>
2019-02-03 07:16:41 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -110,7 +110,7 @@ $(function() {
     get: 'user',
     limit: 6,
     resolution: 'standard_resolution',
-    userId: '8987997105',
+    userId: '8987997106',
     accessToken: '8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9',
     template:
       '<li class="instagram-item"><a href="{{link}}" aria-label="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></li>'
